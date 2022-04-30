8f139147c7c6f523d11f1f602848047b812b1cc4
MuratKaan98 <murat-kaan98@hotmail.com>
2022-05-01 00:50:57 +0300

    Canny Edge Detector

---
+++ Canny-Edge/utils.py
@@ -0,0 +1,41 @@
+import numpy as np
+import skimage
+import matplotlib.pyplot as plt 
+import matplotlib.image as mpimg
+import os
+import scipy.misc as sm
+
+def rgb2gray(rgb):
+#Basicially rgb2gray function. But this definition takes every channel(RGB) as seperated and then turn them to gray
+    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+
+    return gray
+
+
+def load_data(dir_name = 'test_imgs'):   #Loading data from 'test_imgs' folder
+    '''
+    Load images from the "test_imgs" directory
+    Images are in JPG and we convert it to gray scale images
+    '''
+    imgs = []
+    for filename in os.listdir(dir_name):
+        if os.path.isfile(dir_name + '/' + filename):
+            img = mpimg.imread(dir_name + '/' + filename)
+            img = rgb2gray(img)
+            imgs.append(img)
+    return imgs
+
+
+def visualize(imgs, format=None, gray=False):   #For visualizing 
+    plt.figure(figsize=(20, 40))
+    for i, img in enumerate(imgs):
+        if img.shape[0] == 3:
+            img = img.transpose(1,2,0)
+        plt_idx = i+1
+        plt.subplot(2, 2, plt_idx)
+        plt.imshow(img, format)
+    plt.show()
+    
+
+    
